10dfd940a9edc68e9aa9fff5acd35ac3717f55bd
Andu <alex.banu14@gmail.com>
2017-10-21 22:36:46 +0300

    Teams index added

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -2,27 +2,31 @@
 
 @section('content')
 
-    @php($teams = collect(['']))
+    @php($teams = \App\Models\Team::all())
 
-    @foreach($teams->chunk(3) as $collection)
-    <div class="row">
-        @foreach($collection as $team)
-        <div class="col-md-4">
-            <div class="thumbnail">
-                <div class="thumb">
-                    <img src="assets/images/placeholder.jpg" alt="">
+    @if($teams ?? false)
+        @foreach($teams->chunk(4) as $collection)
+            <div class="row">
+                @foreach($collection as $team)
+                <div class="col-xs-6 col-md-3">
+                    <div class="thumbnail">
+                        <div class="thumb">
+                            <img src="assets/images/placeholder.jpg" alt="">
 
-                    <a href="#" class="caption-overflow"></a>
+                            <a href="#" class="caption-overflow"></a>
+                        </div>
+
+                        <div class="caption text-center">
+                            <h5 class="text-semibold no-margin">{{ $team->display_name }}</h5>
+                            <p class="text-muted mb-15 mt-5">{{ $team->description }}</p>
+                        </div>
+                    </div>
                 </div>
-
-                <div class="caption text-center">
-                    <h5 class="text-semibold no-margin">{{ $team->display_name }}</h5>
-                    <p class="text-muted mb-15 mt-5">{{ $team->description }}</p>
-                </div>
+                @endforeach
             </div>
-        </div>
         @endforeach
-    </div>
-    @endforeach
+    @else
+        <h1>You are not working in any team</h1>
+    @endif
 
 @endsection
